ee2054d336b4e20cc0bceb112d11347bd2b63a6e
Glenn Smith <couleeapps@gmail.com>
2016-08-24 14:10:31 -0400

    Start of totals

--- javascript/bill.js
+++ javascript/bill.js
@@ -1,4 +1,5 @@
 function Bill() {
+    //Start off with empty lists
     this.items = [];
     this.people = [];
 
@@ -10,42 +11,52 @@ function Bill() {
     this.addPerson("Test Guy 2");
     this.addPerson("Test Guy 3");
 
-    this.addItem("Test Food", "$42", 1);
-    this.addItem("Test Other Food", "$24", 2);
+    this.addItem("Test Food", "42", 1);
+    this.addItem("Test Other Food", "24", 2);
 }
 
+//Add a food item to the bill (and page)
 Bill.prototype.addItem = function(name, price, personNumber) {
     var item = new Bill.Item(this);
 
+    //Set up the item's fields
     item.name = name;
     item.price = price;
     item.person = this.people[personNumber];
 
+    //And actually add it to the page
     item.appendJQ();
 };
 
+//Remove a food item from the bill
 Bill.prototype.deleteItem = function(item) {
+    //Remove from the page
     item.removeJQ();
 
     //Remove this from our items
     this.items.splice(this.items.indexOf(item), 1);
 };
 
+//Add a person to the bill (and page)
 Bill.prototype.addPerson = function(name) {
     var person = new Bill.Person(this);
 
+    //Set up the person's fields
     person.name = name;
 
     //Add them to the list
     person.id = this.nextPerson ++;
     this.people[person.id] = person;
 
+    //And actually add it to the page
     person.appendJQ();
 };
 
-Bill.prototype.deletePerson = function(item) {
-    item.removeJQ();
+//Remove a person from the bill
+Bill.prototype.deletePerson = function(person) {
+    //Remove from the page
+    person.removeJQ();
 
     //Remove this from our items
-    this.people.splice(this.people.indexOf(item), 1);
+    this.people.splice(this.people.indexOf(person), 1);
 };
--- javascript/index.js
+++ javascript/index.js
@@ -2,27 +2,41 @@ $(function() {
     var bill = new Bill();
 
     $("#addItemButton").click(function() {
+        //Get the values from the text boxes
         var nameField = $("#addItemName");
         var priceField = $("#addItemPrice");
         var personField = $("#addItemPerson");
-
         var name = nameField.val();
         var price = priceField.val();
         var person = personField.val();
 
+        //Clear them so we can use them next time
         nameField.val("");
         priceField.val("");
         personField.val("");
 
+        //Add the item to the bill
         bill.addItem(name, price, person);
+
+        //Update how much everyone has to pay
+        calculateResults();
     });
     $("#addPersonButton").click(function() {
+        //Get the value from the text box
         var nameField = $("#addPersonName");
-
         var name = nameField.val();
 
+        //Clear it so we can use it again
         nameField.val("");
 
+        //Add the person to the bill
         bill.addPerson(name);
-    })
+
+        //Update how much everyone has to pay
+        calculateResults();
+    });
+
+    function calculateResults() {
+
+    }
 });
