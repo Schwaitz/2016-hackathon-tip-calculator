c1b8ab155ae7ee703f4cb108ac5306c64cb0069f
Glenn Smith <couleeapps@gmail.com>
2016-08-24 11:24:20 -0400

    More data

--- javascript/index.js
+++ javascript/index.js
@@ -3,8 +3,34 @@ $(function() {
     var billBody = $("#bill");
 
     $("#addButton").click(function() {
+        var name = prompt("Name");
         var price = prompt("Price");
+        var number = prompt("Person #");
 
-        billBody.append($("<div></div>").text(price));
+        billBody.append(createBillItem(name, price, number));
+
     });
 });
+
+function createBillItem(name, price, person) {
+    var item = $("<div></div>");
+    item.attr({
+        "data-name": name,
+        "data-price": price,
+        "data-person": person
+    });
+    item.text("Person #" + person + " owes " + price + " for the " + name);
+    item.addClass("billItem");
+    item.append(
+        $("<button></button>")
+            .text("Delete")
+            .click(function() {
+                deleteBillItem(item);
+            })
+    );
+    return item;
+}
+
+function deleteBillItem(item) {
+    item.remove();
+}
