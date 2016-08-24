2fe83f52a3fc6cd410085496283b0de06fcec953
Justin Schwaitzberg <justinschwaitzberg@gmail.com>
2016-08-24 16:23:07 -0400

    removed ajax folder / lobby validation

---
+++ php/joinLobby.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+require_once("./db-cont.php");
+
+$lobby = $_POST["name"];
+$pass = $_POST["password"];
+
+
+
+   $conn = new mysqli(host, username, password, database);
+   
+   
+   if ($conn->connect_error) {
+    
+     echo(json_encode("ConnectionError"));
+
+    
+   }
+   
+   $hash_check = password_hash($pass, PASSWORD_DEFAULT);
+   
+   
+  $sql = "SELECT password from lobby WHERE name='$lobby' LIMIT 1";
+  
+	$result = $conn->query($sql);
+  
+  $pa = $result->fetch_assoc();
+	
+  if(password_verify($pass, $pa['password'])){
+    
+     echo(json_encode("true"));
+
+    
+    } else{
+      
+      echo(json_encode("false"));
+
+      
+}
+  
+   $conn->close();
+
+?>
