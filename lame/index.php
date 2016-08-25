d8dd94aaa978e5573a0d2dd1fb4a003ac0f3e0ef
Justin Schwaitzberg <justinschwaitzberg@gmail.com>
2016-08-25 10:41:11 -0400

    Meme Magic part 5.6 FINAL

--- lame/index.php
+++ lame/index.php
@@ -1,7 +1,6 @@
 <?php
 
 
-echo("<html><body style='color: rgb(100, 255, 255);'>");
 //Lazy, but pretty
 echo("<pre style='font-family: \"Comic Sans MS\", cursive, sans-serif; font-size: 25px; color: rgb(150, 255, 60)'>");
 
@@ -9,5 +8,3 @@ echo("<pre style='font-family: \"Comic Sans MS\", cursive, sans-serif; font-size
 passthru("cd " . __DIR__ . " && git pull");
 
 echo("</pre>");
-
-echo("</body></html>");
