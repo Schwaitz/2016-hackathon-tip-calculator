d82c92490bfce728835264f73f8aa97ec7f43954
Justin Schwaitzberg <justinschwaitzberg@gmail.com>
2016-08-25 10:15:15 -0400

    Meme Magic part 3

--- lame/index.php
+++ lame/index.php
@@ -1,7 +1,7 @@
 <?php
 
 //Lazy, but pretty
-echo("<pre style='font-family: \"Comic Sans MS\", cursive, sans-serif'>");
+echo("<pre style=\"font-family: \"Comic Sans MS\", cursive, sans-serif;\">");
 
 //Laziness to the max
 passthru("cd " . __DIR__ . " && git pull");
